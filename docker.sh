a687f26e96258d3d85c01955d6d3ce30eb8d0f33
Nick Parsons <nparsons08@gmail.com>
2018-08-09 15:48:27 -0600

    WIP

---
+++ docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# build the docker image
+docker build -t boilerplate-api/api .
+
+# list all docker images
+docker images
+
+# run the docker image
+docker run -p 8080:3000 -d boilerplate-api/api
+
+# wait for app to start
+sleep 5
+
+# open in browser
+open http://localhost:8080
